6c745a6f78e89c17c179178bac4086af8a2f56ae
Austin Lasseter <austinlasseter@gmail.com>
2020-12-04 12:00:53 -0500

    updated the func

--- python_practice/collatz.py
+++ python_practice/collatz.py
@@ -8,12 +8,28 @@ def collatz(*arg):
     Parameters: any positive integer n
     Returns: List of Collatz permutations, ending in 1
     '''
-    return arg[0]
+    n = arg[0]
+    try:
+        if n%2==0:
+            return n/2
+        if n%2==1:
+            return (n*3)+1
+    except:
+        return 'invalid entry'
 
+def masterfunc(*arg):
+    x = collatz(arg[0])
+    output=[x]
+    if isinstance(x, int):
+        while x !=1:
+            x=collatz(x)
+            output.append(x)
+        return output
+    else:
+        return output
 
 # Test with appropriate inputs
 if __name__=='__main__':
     for arg in [12, 19, 'foobar']:
-        output = collatz(arg)
-        print(arg)
-        print(output)
+        output = masterfunc(arg)
+        print(arg, output)
